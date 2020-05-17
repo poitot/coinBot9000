531c74b98eeda9509d10b87c7e9a2b81135975b8
Connor <connortennant97@gmail.com>
2020-05-17 01:57:06 +0100

    added functionality to allow user to get previous prices

--- index.js
+++ index.js
@@ -10,8 +10,29 @@ client.once('ready', () => {
 
 client.on('message', message => {
 	if (message.content.includes("?")) {
-        console.log("hit");
-        
+        let reg = /[0-9]+[dwmy]+/;
+        let matches = message.content.match(reg);
+        if (matches != null)
+        {
+            let endIndex = matches[0].length - 1
+            let dwmy = matches[0].charAt(endIndex);
+            console.log(endIndex);
+            let num = matches[0].split(/([dwmy])/)[0];
+            console.log(num + " " + dwmy);
+            let date = getDate(num, dwmy);
+            let coin = message.content.match(/(\?\w*)/)[0].replace("?", "");
+
+
+            console.log(matches);
+            let m = getSpot(coin + "-gbp", date).then(function (msg) {
+                console.log("sending message: " + msg);
+                message.channel.send(msg).catch(function (error) {
+                    console.log(error);
+                })
+            });
+        }
+        else {
+            console.log("null")
         // send back spot price of btc from  coinbase API in GBP
         let m = getSpot(message.content.replace('?', '') + "-gbp").then(function (msg) {
             console.log("sending message: " + msg);
@@ -19,7 +40,7 @@ client.on('message', message => {
                 console.log(error);
             })
         });
-        console.log("client.on.message " + m);
+    }
         
 
     }
@@ -43,13 +64,21 @@ client.on('message', message => {
         });
         console.log("client.on.message " + m);
     }
+
     
 });
 
 
 
-async function getSpot(curr) {
-    let response = await fetch("https://api.coinbase.com/v2/prices/" + curr +"/spot");
+async function getSpot(curr, date) {
+    if (date === null)
+    {
+        var response = await fetch("https://api.coinbase.com/v2/prices/" + curr +"/spot");
+    }
+    else {
+        var response = await fetch("https://api.coinbase.com/v2/prices/" + curr +"/spot"+"?date="+date);
+    }
+    
     let res = await fetch("https://api.coinbase.com/v2/prices");
     if (res.ok) {
         let j = await res.json();
@@ -77,5 +106,25 @@ function roundToTwo(num) {
     return +(Math.round(num + "e+2")  + "e-2");
 }
 
+function getDate(nAd, lAd) {
+    let date = new Date();
+    switch(lAd) {
+        case "d":
+            date.setDate(date.getDate() - nAd);
+            break;
+        case "w":
+            date.setDate(date.getDate() - (7 * nAd));
+            break;
+        case "m":
+            date.setMonth(date.getMonth() -nAd);
+            break;
+        case "y":
+            date.setFullYear(date.getFullYear() -nAd);
+            break;
+    }
+    let date_str = date.toISOString().split("T")[0];
+    console.log(date_str);
+    return date_str;
+}
 
 client.login(process.env.TOKEN);
